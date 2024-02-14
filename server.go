5a9f3e63a04e877db4c512f21f8668628ba3062b
ref <56499429+referefref@users.noreply.github.com>
2024-02-15 00:10:32 +0800

    features and bugs
    Added verbose output to terminal
    FIxed issue with duplicate headers
    Added default log output file

--- server.go
+++ server.go
@@ -1,144 +1,117 @@
 package main
 
 import (
+    "crypto/rand"
     "fmt"
+    "log"
     "net/http"
     "net/http/httputil"
-    "log"
-    "strings"
-    "crypto/rand"
     "os"
+    "strings"
     "time"
+    color "github.com/TwiN/go-color"
 )
 
-// no authentication for the following requests 
 func whiteList(url string) bool {
-	
-	switch url[1:] {
-		case
-			"logon.htm",
-			"invalid.htm",
-			"logo.gif",
-			"styles.css",
-			"run.gif":
-			return true
-	}
-	return false
-}
-
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	
-	w.Header().Set("Cache-Control", "no-cache")
-	w.Header().Set("Server", "Intel(R) Active Management Technology 9.1.34")
-
-	p := r.URL.Path
-
-	// AMT will redirect on /
-	if p == "/" {
-		http.Redirect(w, r, "logon.htm", 301)
-	}
-
-	content := p[1:]
-
-	// don't do auth for some resources
-	
-	if whiteList(p) == false {
-		if doAuth(w, r) == false {
-			content = "invalid.htm"
-		}
-	} 
-	
-	http.ServeFile(w, r, "static/" + content)
+    switch url[1:] {
+    case "logon.htm", "invalid.htm", "logo.gif", "styles.css", "run.gif":
+        return true
+    }
+    return false
 }
 
 func parseAuthHeader(r *http.Request) map[string]string {
-	auth := make(map[string]string)
-
-	h := r.Header.Get("Authorization")
-
-	if len(h) > 0 {
-		
-		
-		s := strings.Split(h, ",")
-		// neaten things up
-
-		for _, v := range(s) {
-			z := strings.Split(v, "=")
-			z0 := strings.Trim(z[0], "\" ")
-			z1 := strings.Trim(z[1], "\" ")
-			auth[z0] = z1
-		}
-	}
-
-	return auth
+    auth := make(map[string]string)
+    h := r.Header.Get("Authorization")
+    if len(h) > 0 {
+        s := strings.Split(h, ",")
+        for _, v := range s {
+            z := strings.Split(v, "=")
+            z0 := strings.Trim(z[0], "\" ")
+            z1 := strings.Trim(z[1], "\" ")
+            auth[z0] = z1
+        }
+    }
+    return auth
 }
 
-func doAuth(w http.ResponseWriter, r *http.Request) bool {
+func doAuth(w http.ResponseWriter, r *http.Request) (bool, bool) {
+    auth := parseAuthHeader(r)
+    fmt.Println(color.Ize(color.Blue, "Login attempt: "), r.RemoteAddr)
+    fmt.Println(color.Ize(color.Blue, "Credentials: "), auth)
+    
+    if auth["Digest username"] == "admin" && auth["response"] == "" {
+        return true, false
+    }
 
-	auth := parseAuthHeader(r)
-	
-	// user is admin, response is null someone is doing something naughty!
-	if auth["Digest username"] == "admin" && auth["response"] == "" {
-		return true
-	}
+    digest := "C10000000000000000000000000000"
+    b := make([]byte, 32)
+    rand.Read(b)
+    nonce := fmt.Sprintf("%x", b)[:32]
 
-	digest := "C10000000000000000000000000000"
+    w.Header().Set("WWW-Authenticate", `Digest realm="Digest:`+digest+`", nonce="`+nonce+`", stale="false", qop="auth"`)
+    w.WriteHeader(http.StatusUnauthorized)
+    return false, true
+}
 
-	b := make([]byte, 32)
-	rand.Read(b)
-	nonce := fmt.Sprintf("%x", b)[:32]
+func handler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Cache-Control", "no-cache")
+    w.Header().Set("Server", "Intel(R) Active Management Technology 9.1.34")
 
-	w.Header().Set("WWW-Authenticate", "Kerberos")
-	w.Header().Set("WWW-Authenticate", "Negotiate")
-	w.Header().Set("WWW-Authenticate", "Digest realm=\"Digest:" + digest + "\", nonce=\"" + nonce +   "\",stale=\"false\",qop=\"auth\"")
-	
-	// need to be explicit otherwise WriteHeader will override content type
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(401)
-	return false
+    p := r.URL.Path
+    if p == "/" {
+        http.Redirect(w, r, "logon.htm", 301)
+        return 
+    }
 
+    content := p[1:]
+    if !whiteList(p) {
+        authSuccess, responseWritten := doAuth(w, r)
+        if !authSuccess {
+            if !responseWritten {
+                http.ServeFile(w, r, "static/invalid.htm")
+            }
+            return 
+        }
+    }
+
+    http.ServeFile(w, r, "static/"+content)
 }
 
 func Log(handler http.Handler, logFile string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        rAddress := color.Ize(color.Yellow, "Remote:"+r.RemoteAddr)
+        t := time.Now().Format(time.RFC850)
+        dump, err := httputil.DumpRequest(r, false)
+        if err == nil {
+            entry := fmt.Sprintf("%s\n%s\n%s", t, rAddress, dump)
+            f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+            if err != nil {
+                log.Println(color.Ize(color.Red, "error opening log file:"), err)
+            }
+            defer f.Close()
 
-		//log.Println(r.RemoteAddr, r.Method, r.URL)
-		rAddress := "Remote:" + r.RemoteAddr
-
-		t := time.Now().Format(time.RFC850)
-
-		dump, err := httputil.DumpRequest(r, false)
-
-		if err == nil {
-			
-			entry := fmt.Sprintf("%s\n%s\n%s", t, rAddress, dump)
-
-			f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				log.Println("error ", err)
-			}
-			defer f.Close()
-
-			_, err = f.WriteString(entry)
-			if err != nil {
-				log.Println("error writing: ", err)
-			}
-
-		}
-
-		handler.ServeHTTP(w, r)
-	})
+            _, err = f.WriteString(entry)
+            if err != nil {
+                log.Println(color.Ize(color.Red, "error writing to log file:"), err)
+            }
+        }
+        handler.ServeHTTP(w, r)
+    })
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("usage: ./server <log file>")
-		os.Exit(1)
-	}
+    logFile := "amthoneypot.log"
+    if len(os.Args) > 1 {
+        logFile = os.Args[1]
+    }
 
-	logFile := os.Args[1]
+    port := ":16992"
+    fmt.Println(color.Ize(color.Green, "Starting server on port"+port))
 
-    http.HandleFunc("/", handler) 
-    http.ListenAndServe(":16992", Log(http.DefaultServeMux, logFile))
-} 
+    http.HandleFunc("/", handler)
+    err := http.ListenAndServe(port, Log(http.DefaultServeMux, logFile))
+    if err != nil {
+        log.Fatal(color.Ize(color.Red, "Server failed to start:"), err)
+    }
+}
